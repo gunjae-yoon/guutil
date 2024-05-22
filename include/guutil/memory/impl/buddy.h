79f8e3ed9015d52fe9dffee79b8998dc30c704d7
Gunjae Yoon <gjyoon@plaif.com>
2024-05-22 11:08:12 +0900

    Update MemoryManager and Buddy classes

---
+++ include/guutil/memory/impl/buddy.h
@@ -0,0 +1,32 @@
+#ifndef __memory_impl_buddy_h__
+#define __memory_impl_buddy_h__
+
+#include <guutil/memory/memorymanager.h>
+
+class Buddy : public MemoryManager {
+private:
+	struct Block {
+		uint64_t size;
+		bool free;
+		Block* next;
+	};
+
+public:
+	Buddy(uint64_t size);
+	~Buddy();
+
+private:
+	void* _allocate(const uint64_t size) override;
+	void _deallocate(void* ptr) override;
+	
+	Block* create(int8_t* base, uint64_t offset, const uint64_t size);
+	Block* find(const uint64_t size);
+	void split(Block* block, uint64_t size);
+	void merge(Block* block);
+
+private:
+	Block* head;
+	int8_t* baseMemory;
+};
+
+#endif
---
+++ src/guutil/version.cpp
@@ -0,0 +1,10 @@
+#include <guutil/version.h>
+
+namespace guutil::Version {
+    const uint64_t major = 0;
+    const uint64_t minor = 0;
+    const uint64_t patch = 1;
+    const char* hashcode = "85e5191";
+    const char* updated = "2024/05/22 10:49:55";
+    const std::string full = std::to_string(major) + "." + std::to_string(minor) + "." + std::to_string(patch) + " (updated: 2024/05/22 10:49:55, hash: 85e5191)";
+}
