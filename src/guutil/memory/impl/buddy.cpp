9a70609ccc5a4123825ab1ca14e5ff7662b6aa4c
Gunjae Yoon <gjyoon@plaif.com>
2024-05-24 14:02:08 +0900

    Update log messages to use guutil::log::Logger

--- src/guutil/memory/impl/buddy.cpp
+++ src/guutil/memory/impl/buddy.cpp
@@ -4,20 +4,22 @@
 
 namespace guutil {
 	Buddy::Buddy(uint64_t size) : MemoryManagerImpl(size) {
-		printf("Buddy::Buddy(%lu)\n", size);
+		logger.trace(__FUNCTION__, size);
 		baseMemory = static_cast<int8_t*>(std::malloc(size));
 		head = create(baseMemory, 0, size);
 	}
 
 	Buddy::~Buddy() {
+		logger.trace(__FUNCTION__);
 		std::free(baseMemory);
 	}
 
 	void* Buddy::allocate(const uint64_t size) {
-		printf("Buddy::allocate(%lu)\n", size);
+		logger.trace(__FUNCTION__, size);
 		const uint64_t blockSize = std::pow(2, std::ceil(std::log2(size))); // Note: round up to the nearest power of two
 		Block* block = find(blockSize);
 		if (block == nullptr) {
+			logger.error("out of resources", "(failed to find free block)");
 			return nullptr;
 		}
 		block->free = false;
@@ -26,8 +28,9 @@ namespace guutil {
 	}
 
 	void Buddy::deallocate(void* ptr) {
-		printf("Buddy::deallocate(%p)\n", ptr);
+		logger.trace(__FUNCTION__, ptr);
 		if (ptr == nullptr) {
+			logger.error("invalid argument", "(tried to free nullptr)");
 			return;
 		}
 		Block* block = static_cast<Block*>(ptr) - 1;
@@ -36,7 +39,7 @@ namespace guutil {
 	}
 
 	Buddy::Block* Buddy::create(int8_t* base, const uint64_t offset, const uint64_t size) {
-		printf("Buddy::create(%lu)\n", size);
+		logger.trace(__FUNCTION__, static_cast<void*>(base), offset, size);
 		Block* block = reinterpret_cast<Block*>(base + offset);
 		block->size = size;
 		block->free = true;
@@ -45,10 +48,11 @@ namespace guutil {
 	}
 
 	Buddy::Block* Buddy::find(const uint64_t size) {
-		printf("Buddy::find(%lu)\n", size);
+		logger.trace(__FUNCTION__, size);
 		Block* current = head;
 		while (current != nullptr) {
 			if (current->free && current->size >= size) {
+				logger.verbose("find free block", "( req:", size, "find:", current->size, ")");
 				return current;
 			}
 			current = current->next;
@@ -57,7 +61,7 @@ namespace guutil {
 	}
 
 	void Buddy::split(Block* block, const uint64_t size) {
-		printf("Buddy::split(%p, %lu)\n", block, size);
+		logger.trace(__FUNCTION__, block, size);
 		while (block->size > size) {
 			uint64_t newSize = block->size / 2;
 			Block* newBlock = create(baseMemory, reinterpret_cast<int8_t*>(block) - baseMemory + newSize, newSize);
@@ -68,7 +72,7 @@ namespace guutil {
 	}
 
 	void Buddy::merge(Block* block) {
-		printf("Buddy::merge(%p)\n", block);
+		logger.trace(__FUNCTION__, block);
 		Block* current = head;
 		while (current != nullptr) {
 			if (current->free && current->next && current->next->free &&
--- src/guutil/memory/impl/memorymanager_impl.cpp
+++ src/guutil/memory/impl/memorymanager_impl.cpp
@@ -1,6 +1,8 @@
 #include <guutil/memory/impl/memorymanager_impl.h>
 
 namespace guutil {
+	log::Logger MemoryManagerImpl::logger("memory", log::Level::ALL, log::Target::CONSOLE);
+
 	MemoryManagerImpl::MemoryManagerImpl(const uint64_t size) : capacity(size) {
 	}
 
